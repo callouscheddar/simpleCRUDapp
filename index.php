194fbd04beff89f3af39a9e49d3d2ffb57b60746
Anthony <bonsilaoanthony@gmail.com>
2022-07-26 12:41:31 -0400

    View all users in index

--- index.php
+++ index.php
@@ -11,9 +11,33 @@ include __DIR__ . '/users.php';
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-    <title>Document</title>
+    <title>Basic Crud App</title>
 </head>
 <body>
-    <?= allUsers(); ?>
+<h1>Users</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <td>Id</td>
+            <td>Name</td>
+            <td>Username</td>
+            <td>Email</td>
+            <td>Phone</td>
+            <td>Website</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach(allUsers() as $user): ?>
+            <tr>
+                <td><?= $user['id'] ?></td>
+                <td><?= $user['name'] ?></td>
+                <td><?= $user['username'] ?></td>
+                <td><?= $user['email'] ?></td>
+                <td><?= $user['phone'] ?></td>
+                <td><?= $user['website'] ?></td>
+            </tr>
+        <?php endforeach ?>
+    </tbody>
+</table>
 </body>
 </html>
--- users.php
+++ users.php
@@ -2,20 +2,33 @@
 
 function allUsers()
 {
-    $users = json_decode(file_get_contents(__DIR__ . '/users.json'), true);
-    echo '<pre>';
-    var_dump($users);
-    echo '</pre>';
+    return json_decode(file_get_contents(__DIR__ . '/users.json'), true);
 }
 
 function findUser($id)
 {
-
+    $users = allUsers();
+    foreach ($users as $user) {
+        if ($user['id'] == $id) {
+            return $user;
+        }
+    }
+    return '';
 }
 
 function createUser($data)
 {
-
+    // Validate $data
+    if (empty($data['id']) || empty($data['name'])
+    || empty($data['username']) || empty($data['email'])
+    || empty($data['phone']) || empty($data['website'])) {
+        echo 'Not valid';
+        return;
+    }
+    // Add data to users, then replace json file with new data
+    $users = allUsers();
+    $users[] = $data;
+    file_put_contents(__DIR__ . '/users.json', json_encode($users));
 }
 
 function updateUser($id, $data)
